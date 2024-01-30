e2c9cba5be69dbef1c39d7604b21a36c6c9b5dab
leelasaranyaM <147148255+leelasaranyaM@users.noreply.github.com>
2024-01-30 15:08:31 +0530

    Update genset.py

--- smart controller/ps/genset.py
+++ smart controller/ps/genset.py
@@ -6,17 +6,15 @@ from cryptography.fernet import Fernet
 
 stored_value=0.2
 generator_cost=0.2
-# Load encryption key from the file
+
 with open('key.key', 'rb') as key_file:
     key = key_file.read()
 
 cipher_suite = Fernet(key)
 
-# Encrypt message function
 def encrypt_message(message):
     return cipher_suite.encrypt(message.encode()).decode()
 
-# Decrypt message function
 def decrypt_message(encrypted_message):
     return cipher_suite.decrypt(encrypted_message.encode()).decode()
 
@@ -43,49 +41,37 @@ while step < 24:
         print("Stored value retrieved from toplevel:", stored_value)
     else:
         print("Stored value retrieval failed or not found.")
-        # Default value or error handling in case retrieval fails
-        stored_value = 0.2  # Set default value as 0.2 if retrieval fails
+        stored_value = 0.2
 
-    # Encrypt the data before sending
     encrypted_data = encrypt_message(json.dumps({"component_id": "generator", "generator_cost": stored_value}))
 
-    # Send the encrypted data to the server
     requests.post(f'{smart_controller_url}/update_data', json={'encrypted_data': encrypted_data})
     time.sleep(10)
 
-    # Retrieve the encrypted response from the server
     response = requests.get(f'{smart_controller_url}/generator_response')
     encrypted_response = response.json().get('encrypted_data', '')
 
-    # Decrypt the received encrypted response
     decrypted_response = decrypt_message(encrypted_response)
     energy_management_data = json.loads(decrypted_response)
 
-    # Extract and print the decrypted data
     print("Generator Energy Used:", energy_management_data.get('generator_energy_used', 'Not available'))
 
     from web3 import Web3
     import json
     from web3.middleware import geth_poa_middleware
 
-    # Create an instance of the Web3 class
     w3 = Web3(Web3.HTTPProvider('http://127.0.0.1:8545'))
-    # Add the Geth PoA middleware
     w3.middleware_onion.inject(geth_poa_middleware, layer=0)
-    # Set the default account
     w3.eth.default_account = "0x0Fc768a5837b622bB36766177Dbc4Fa19034D592"
     abi = json.loads('[{"inputs":[{"internalType":"string","name":"element","type":"string"}],"name":"battery","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"battery_data","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"string","name":"element","type":"string"}],"name":"genset","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"genset_data","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"string","name":"element","type":"string"}],"name":"grid","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"grid_data","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"string","name":"element","type":"string"}],"name":"load","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"load_data","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"string","name":"element","type":"string"}],"name":"renewable","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"renewable_data","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"show_battery","outputs":[{"internalType":"string[]","name":"","type":"string[]"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"show_genset","outputs":[{"internalType":"string[]","name":"","type":"string[]"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"show_grid","outputs":[{"internalType":"string[]","name":"","type":"string[]"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"show_load","outputs":[{"internalType":"string[]","name":"","type":"string[]"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"show_renewable","outputs":[{"internalType":"string[]","name":"","type":"string[]"}],"stateMutability":"view","type":"function"}]')
     address = Web3.to_checksum_address("0x93CBBf865A4926A028e5d4635f879a4ab107007D")
     contract = w3.eth.contract(address=address,abi=abi)
 
     
-    # Transact the setGreeting function
     tx_hash = contract.functions.genset(str(stored_value)).transact({'from': w3.eth.default_account})
 
-    # Wait for the transaction to be mined, and get the transaction receipt
     tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
 
-    # Print the transaction receipt
     print("BlockNumber:",tx_receipt['blockNumber'])
 
     call2=contract.functions.show_genset().call()
